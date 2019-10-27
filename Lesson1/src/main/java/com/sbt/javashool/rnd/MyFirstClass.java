dfe42ac43894de479886433964598b3e4dfbac0e
Mixel2142 <efremov20081@gmail.com>
2019-10-27 16:37:49 +0300

    add class MyFirstClass use import package

---
+++ Lesson1/src/main/java/com/sbt/javashool/rnd/MyFirstClass.java
@@ -0,0 +1,7 @@
+package com.sbt.javashool.rnd;
+
+public class MyFirstClass {
+    public static void printHelloWorld() {
+    System.out.println("Hello World!");
+    }
+}
---
+++ Lesson1/src/main/java/main.java
@@ -0,0 +1,7 @@
+import com.sbt.javashool.rnd.MyFirstClass;
+
+public class main {
+    public static void main(String[] args) {
+        MyFirstClass.printHelloWorld();
+    }
+}
