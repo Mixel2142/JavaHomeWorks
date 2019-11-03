c7bdc0bad3553896a2ab802ccecaf4cab6e8339b
Mixel2142 <efremov20081@gmail.com>
2019-11-03 10:01:19 +0300

    add files and dir for lesson2

---
+++ Lesson2/src/main/java/com/sbt/Lesson2.java
@@ -0,0 +1,26 @@
+package com.sbt;
+
+public class Lesson2 {
+    public static final boolean RESIDENT = true;
+    public static final boolean NOT_RESIDENT = false;
+    private String firstName;
+    private String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public  Lesson2(
+}
