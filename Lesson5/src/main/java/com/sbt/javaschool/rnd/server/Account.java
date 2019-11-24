46a7425557836d27848b9c945b3bd3f22ec4953d
efrem <efremov20081@gmail.com>
2019-11-24 15:54:06 +0300

    add class Account

---
+++ Lesson5/src/main/java/com/sbt/javaschool/rnd/server/Account.java
@@ -0,0 +1,13 @@
+package com.sbt.javaschool.rnd.server;
+
+public class Account {
+    String name;
+    Integer password;
+    Integer money;
+
+    public Account(String name, Integer password, Integer money) {
+        this.name = name;
+        this.password = password;
+        this.money = money;
+    }
+}
--- Lesson5/src/main/java/com/sbt/javaschool/rnd/server/ServerImpl.java
+++ Lesson5/src/main/java/com/sbt/javaschool/rnd/server/ServerImpl.java
@@ -2,23 +2,29 @@ package com.sbt.javaschool.rnd.server;
 
 import com.sbt.javaschool.rnd.exceptions.*;
 import com.sbt.javaschool.rnd.server.Server;
+import javafx.util.Pair;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+
 import java.util.Map;
 
 public class ServerImpl implements Server {
-    Map<String, Integer> dataBase = new HashMap<>();
+    Map<String, Account> dataBase = new HashMap();
 
     {
-        dataBase.put("Ivan", 1234);
-        dataBase.put("Masha", 4321);
-        dataBase.put("Roma", 1997);
+        String id = "Ivan";
+        dataBase.put(id, new Account("Ivan", 1234, 1000));
+        id = "Masha";
+        dataBase.put(id, new Account("Masha", 4321, 2000));
+        id = "Roman";
+        dataBase.put(id, new Account("Roman", 1997, 3000));
     }
 
     @Override
-    public boolean isPinCodeExist(String accaunt,String password) throws PinCodeIsNotExistExeption {
+    public boolean isPinCodeExist(String account, String password) throws PinCodeIsNotExistExeption {
 
-        if (dataBase.get(accaunt) == Integer.parseInt(password)) {
+        if (dataBase.get(account).password == Integer.parseInt(password)) {
             return true;
         }
         throw new PinCodeIsNotExistExeption("Пароль не верный.");
